c7828a8b86628fc9fa849df83345c39a425c9363
fatihk29 <fatihkrc7@gmail.com>
2021-04-15 21:44:00 +0300

    updated

--- src/screens/ComponentsScreen/index.js
+++ src/screens/ComponentsScreen/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the Components Screen</Text>;
+  const greeting = 'Bye There';
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the Components Screen</Text>
+      <Text>{greeting}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
