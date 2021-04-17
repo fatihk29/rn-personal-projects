6cbbbc1fa848fda24a125b0f11ed4d974cd4a18f
fatihk29 <fatihkrc7@gmail.com>
2021-04-18 00:58:00 +0300

    validation added

--- src/screens/SquareScreen/index.js
+++ src/screens/SquareScreen/index.js
@@ -7,10 +7,22 @@ const COLOR_INCREMENT = 15;
 const reducer = (state, action) => {
   switch (action.colorToChange) {
     case 'red':
+      if (state.red + action.amount > 255 || state.red + action.amount < 0) {
+        return state;
+      }
       return {...state, red: state.red + action.amount};
     case 'green':
+      if (
+        state.green + action.amount > 255 ||
+        state.green + action.amount < 0
+      ) {
+        return state;
+      }
       return {...state, green: state.green + action.amount};
     case 'blue':
+      if (state.blue + action.amount > 255 || state.blue + action.amount < 0) {
+        return state;
+      }
       return {...state, blue: state.blue + action.amount};
     default:
       return state;
@@ -52,15 +64,17 @@ const SquareScreen = () => {
         color="Blue"
       />
       <View
-        style={{
-          height: 150,
-          width: 150,
-          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
-        }}></View>
+        style={[
+          styles.colorBox,
+          {backgroundColor: `rgb(${red}, ${green}, ${blue})`},
+        ]}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  colorBox: {height: 150, width: 150},
+});
 
 export default SquareScreen;
